8f95c1a03d6cf15751dcf7becc51faccc7a5fa91
Reland Boyle <reland.boyle@gmail.com>
2022-08-13 11:08:39 -0400

    Weather fetching

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { FormInputs, WeatherConditions } from '../../models';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-display',
@@ -8,15 +10,34 @@ import { FormInputs, WeatherConditions } from '../../models';
   styleUrls: ['./display.component.scss']
 })
 export class DisplayComponent implements OnInit {
-
   @Input() formInputs!: FormInputs;
   @Input() weatherConditions!: WeatherConditions;
+  cityZip: string = '';
+  weatherKey: string = environment.apiKey;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
 
   ngOnInit(): void {
+    this.weatherConditions.cityZip = '11225';
+    this.cityZip = this.weatherConditions.cityZip;
+    console.log(this.weatherConditions);
 
-    console.log(this.formInputs);
+  }
+
+  getWeather() {
+    const requestUrl = `https://api.weatherapi.com/v1/current.json?key=${this.weatherKey}&q=${this.cityZip}&aqi=no`;
+
+    this.http.get(requestUrl).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (err) => console.error(err),
+      complete: () => {
+        console.log('getWeather COMPLETE');
+      }
+    })
   }
 
 }
