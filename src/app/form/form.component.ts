3cd24c6cb15c2cac2fd37db5d4d60d92accd62b6
relandboyle <reland.boyle@gmail.com>
2022-10-21 20:35:02 -0400

    border-radius: inherit;

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { WeatherService } from '../services/weather.service';
 import { InputValues, WeatherData } from '../types/interfaces';
 import { InputStore } from '../constants/input-values';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -10,22 +11,41 @@ import { InputStore } from '../constants/input-values';
 })
 export class FormComponent implements OnInit {
 
-  cityZip: string = '';
   myData: any = [];
   weather: any;
   inputs: InputValues = InputStore;
+  inputForm!: FormGroup;
 
   @Input() blur = false;
 
 
   constructor(
-    private wxService: WeatherService
+    private wxService: WeatherService,
+    private fb: FormBuilder,
     ) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.inputForm = this.fb.group({
+      units: 'imperial',
+      propDiameter: null,
+      battVoltage: null,
+      motorVoltage: null,
+      airspeed: null,
+      altitude: null,
+      location: '',
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.inputForm.value);
+  }
 
   sendCityZip() {
-    this.wxService.storeCityZip(this.cityZip);
+    this.wxService.storeCityZip(this.inputForm.value.location);
   }
 
 }
