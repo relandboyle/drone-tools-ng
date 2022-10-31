d42a8f7b2a8e939812499db6ff8118d68463cb97
relandboyle <reland.boyle@gmail.com>
2022-10-31 09:01:30 -0400

    Completed units toggle logic

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -27,20 +27,17 @@ export class FormComponent implements OnInit {
   ngOnInit(): void {
     this.initializeForm();
     this.syncValues();
-    // this.syncForks();
   }
 
 
   initializeForm(): void {
     this.inputForm = this.fb.group({
-      airspeedStore: new FormControl(),
       airspeedKph: new FormControl(),
       airspeedKnots: new FormControl(),
       altitude: new FormControl(),
       battVoltage: new FormControl(),
       location: new FormControl(''),
       motorVoltage: new FormControl(),
-      propDiameterStore: new FormControl(),
       propDiameterIn: new FormControl(),
       propDiameterMm: new FormControl(),
       units: new FormControl('imperial'),
@@ -48,126 +45,63 @@ export class FormComponent implements OnInit {
   }
 
 
-  // syncForks(): void {
-  //   const form = this.inputForm;
-
-  //   const inches = form.controls['propDiameterIn'].valueChanges
-  //   const metric = form.controls['propDiameterMm'].valueChanges
-  //   const store = form.controls['propDiameterStore'].valueChanges
-  //   const units = form.controls['units'].valueChanges
-
-  //   inches.pipe(
-  //     mergeWith(metric, units)
-  //   )
-  //   .subscribe(s => console.log(s))
-
-
-  // }
-
-
   syncValues(): void {
     const form = this.inputForm;
-    const noEmit = { onlySelf: true, emitEvent: false };
-    const diameterInput = document.querySelector('.propDiameter');
-
-    const merged = merge(
-      form.controls['propDiameterIn'].valueChanges,
-      form.controls['propDiameterMm'].valueChanges,
-      form.controls['units'].valueChanges
-    );
-
-    merged.subscribe(mergedValues => {
-      console.log('MERGED', mergedValues);
-    })
+    const noEmit = { emitEvent: false };
+    const diameterImperial = form.controls['propDiameterIn'];
+    const diameterMetric = form.controls['propDiameterMm'];
+    const airspeedKnots = form.controls['airspeedKnots'];
+    const airspeedKph = form.controls['airspeedKph'];
+    const units = form.controls['units'];
 
 
-
-    // units.pipe(
-    //   mergeWith(propDiameterIn, propDiameterMm, airspeedKnots, airspeedKph)
-    // )
-    form.valueChanges
-    .subscribe(s => {
-      console.log(s);
-      // if (form.value.units === 'imperial') {
-      //   form.patchValue({
-      //     propDiameterIn: form.value.propDiameterStore || null,
-      //     airspeedStore: form.value.airspeedKnots || null,
-      //     propDiameterMm: parseFloat((form.value.propDiameterStore * 25.4).toFixed(2)) || null,
-      //     airspeedKph: parseFloat((form.value.airspeedKnots * 1).toFixed(1)) || null
-      //   }, noEmit);
-      // }
-      // else if (form.value.units === 'metric') {
-      //   form.patchValue({
-      //     propDiameterMm: form.value.propDiameterStore || null,
-      //     airspeedStore: form.value.airspeedKph || null,
-      //     propDiameterIn: parseFloat((form.value.propDiameterStore / 25.4).toFixed(1)) || null,
-      //     airspeedKnots: parseFloat((form.value.airspeedKph * 1).toFixed(1)) || null
-      //   }, noEmit);
-      //   if (form.controls['propDiameterIn'].pristine) {
-      //     form.patchValue({
-      //     }, noEmit);
-      //   }
-      // }
-    });
-
-
-    form.get('units')?.valueChanges.subscribe(units => {
-      // console.log(units);
-      if (units === 'imperial') {
-        console.log('IMPERIAL')
-        form.controls['propDiameterIn'].markAsPristine();
-        form.controls['airspeedKph'].markAsPristine();
-
-      }
-      else if (units === 'metric') {
-        console.log('METRIC')
-        form.controls['propDiameterMm'].markAsPristine();
-        form.controls['airspeedKnots'].markAsPristine();
-
+    diameterImperial.valueChanges.subscribe(input => {
+      if (form.value.units === 'imperial' && diameterMetric.pristine) {
+        form.patchValue({
+          propDiameterMm: parseFloat((input * 25.4).toFixed(2)) || null
+        }, noEmit);
       }
     });
 
-    // form.valueChanges.subscribe(input => {
-    //   if (form.value.units === 'imperial') {
-    //     form.patchValue({
-    //       airspeedStore: input.airspeedKnots,
-    //       propDiameterStore: input.propDiameterIn,
-    //       airspeedKph: parseFloat((input.airspeedKnots * 1.852).toFixed(1)) || null,
-    //       // propDiameterMm: parseFloat((form.value.propDiameterIn * 25.4).toFixed(1)) || null,
-    //     },
-    //     {
-    //       onlySelf: true, emitEvent: false,
-    //     });
-    //     if (form.controls['propDiameterMm'].pristine) {
-    //       form.patchValue({
-    //         propDiameterMm: parseFloat((form.value.propDiameterIn * 25.4).toFixed(2)) || null,
-    //       },
-    //       {
-    //         onlySelf: true, emitEvent: false,
-    //       });
-    //     }
-    //   }
-    //   else if (form.value.units === 'metric') {
-    //     form.patchValue({
-    //       airspeedStore: input.airspeedKph,
-    //       propDiameterStore: input.propDiameterMm,
-    //       airspeedKnots: parseFloat((input.airspeedKph * 0.539957).toFixed(2)) || null,
-    //       // propDiameterIn: parseFloat((form.value.propDiameterMm / 25.4).toFixed(2)) || null,
-    //     },
-    //     {
-    //       onlySelf: true, emitEvent: false,
-    //     });
-    //     if (form.controls['propDiameterIn'].pristine) {
-    //       form.patchValue({
-    //         propDiameterIn: parseFloat((form.value.propDiameterMm / 25.4).toFixed(1)) || null,
-    //       },
-    //       {
-    //       onlySelf: true, emitEvent: false,
-    //       });
-    //     }
-    //   }
-    // });
 
+    diameterMetric.valueChanges.subscribe(input => {
+      if (form.value.units === 'metric' && diameterImperial.pristine) {
+        form.patchValue({
+          propDiameterIn: parseFloat((input / 25.4).toFixed(1)) || null
+        }, noEmit);
+      }
+    });
+
+
+    airspeedKnots.valueChanges.subscribe(input => {
+      if (form.value.units === 'imperial' && airspeedKph.pristine) {
+        form.patchValue({
+          airspeedKph: parseFloat((input * 1.852).toFixed(1)) || null
+        }, noEmit);
+      }
+    });
+
+
+    airspeedKph.valueChanges.subscribe(input => {
+      if (form.value.units === 'metric' && airspeedKnots.pristine) {
+        form.patchValue({
+          airspeedKnots:  parseFloat((input * 0.539957).toFixed(1)) || null
+        }, noEmit);
+      }
+    });
+
+
+    units.valueChanges.subscribe(change => {
+      if (change === 'imperial') {
+        diameterMetric.markAsPristine();
+        airspeedKph.markAsPristine();
+
+      }
+      else if (change === 'metric') {
+        diameterImperial.markAsPristine();
+        airspeedKnots.markAsPristine();
+      }
+    });
   }
 
 
@@ -184,5 +118,4 @@ export class FormComponent implements OnInit {
   sendLocation(): void {
     this.wxService.storeCityZip(this.inputForm.value.location);
   }
-
 }
