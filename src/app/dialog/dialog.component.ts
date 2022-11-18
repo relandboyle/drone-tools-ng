1e839a6bb36914908389431305f77a473ee7db2d
relandboyle <reland.boyle@gmail.com>
2022-11-17 20:11:33 -0500

    Debugging dialog opening behavior

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,33 +1,20 @@
-import { Component, OnInit, Input, Output, EventEmitter, Inject, InjectionToken } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, Inject, InjectionToken } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.scss']
 })
-export class DialogComponent implements OnInit {
+export class DialogComponent {
 
   data: any;
-  @Input() dialogMessage: string = 'Default Dialog Message String';
-  // @Output() closeDialogEvent = new EventEmitter<string>();
 
   constructor(
-    private dialogRef: MatDialogRef<DialogComponent>,
     @Inject(MAT_DIALOG_DATA) data: InjectionToken<any>
   ) {
     this.data = data;
     console.log(this.data);
   }
-
-  ngOnInit(): void {
-
-    console.log('DialogComponent INIT')
-  }
-
-
-  // closeDialog(event: string) {
-  //   this.closeDialogEvent.emit(event);
-  // }
 
 }
